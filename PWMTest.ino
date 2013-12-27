f23deea8ee753dfec37f88fffb7afd41414f01c3
Cammy-the-block <awesomepossumcraft@gmail.com>
2013-12-27 01:04:54 +0000

    Update PWMTest.ino
    Changed variable names. Added in some comments.

--- PWMTest.ino
+++ PWMTest.ino
@@ -1,59 +1,53 @@
 /*
- RC PulseIn Joystick
- By: Nick Poole
- SparkFun Electronics
- Date: 5
- License: CC-BY SA 3.0 - Creative commons share-alike 3.0
- use this code however you'd like, just keep this license and
- attribute. Let me know if you make hugely, awesome, great changes.
+ Based on Nick Poole's RC PulseIn Joystick
  */
+ 
+int RJX = 6; //Right Joystick X
+int LJY = 5; //Left Joystick Y
+int RJXVal;
+int LJYVal;
+//Max right joystick X = 546 
+//Low right joystick X = - 521
+//Total right joystick X = 1067
 
-int rightJoystickX;
-int leftJoystickY;
-//Max rightJoystickX = 546 
-//Low rightJoystickX = - 521
-//rightJoystickX total = 1067
-
-//Max leftJoystickY = 571
-//Low leftJoystickY = - 517
-//leftJoystickY total = 1088
+//Max left joystick Y = 571
+//Low left joystick Y = - 517
+//Total left joystick Y = 1088
 void setup() {
 
-pinMode(6, INPUT); // Set our input pins as such
-pinMode(5, INPUT); // Set our input pins as such
-Serial.begin(9600); // Pour a bowl of Serial
-
+  pinMode(RJX, INPUT); // Set RSX as an input
+  pinMode(LJY, INPUT); // Set LSY as an input
+  Serial.begin(9600); // Startup serial
 }
 
 void loop() {
   
-  rightJoystickX = pulseIn(6, HIGH, 25000); // Read the pulse width of 
-  leftJoystickY = pulseIn(5, HIGH, 25000); // Read the pulse width of  
-  rightJoystickX = rightJoystickX - 1376;
-  leftJoystickY = leftJoystickY - 1523;
-  if(abs(rightJoystickX) > 50){
-    Serial.println("You pressed rightJoystickX");
+  RJXVal= pulseIn(RJX, HIGH, 25000); // Read the pulse width of right joystick X
+  LJYVal = pulseIn(LJY, HIGH, 25000); // Read the pulse width of left joystick Y
+  RJXVal = RJXVal - 1376; // Centers RSXVal
+  LJYVal = LJXVal - 1523; // Centers LSYVal
+  if(abs(RJXVal) > 50){ //Checks if RJXVal is over 50
+    Serial.println("You pressed RJXVal"); //If so then a message is printed
   }
-  else{
-    rightJoystickX = 0;
+  else{ //Otherwise
+    RJXVal = 0; // Set RJXVal to 0
   }
-  if(abs(leftJoystickY) > 50){
-    Serial.println("You pressed leftJoystickY");
+  if(abs(LSYVal) > 50){ //Checks if LJYVal is over 50
+    Serial.println("You pressed LSYVal"); //If so then a message is printed
   }
-  else{
-    leftJoystickY = 0;
+  else{ //Otherwise
+    LSYVal = 0; // Set LJYVal to 0
   }
-  Serial.print("rightJoystickX :"); 
-  Serial.println(rightJoystickX);
-  Serial.print("leftJoystickY :");
-  Serial.println(leftJoystickY);
-//Serial.println(map(rightJoystickX, 1000,2000,-500,500)); // center at 0
-//Serial.println(map(leftJoystickY, 1000,2000,-500,500)); // center at 0
+  Serial.print("RJXVal :"); 
+  Serial.println(RJXVal);
+  Serial.print("LSYVal :");
+  Serial.println(LSYVal);
+//Serial.println(map(RJXVal, 1000,2000,-500,500)); // center at 0
+//Serial.println(map(LSYVal, 1000,2000,-500,500)); // center at 0
 
-Serial.println(); //make some room
+Serial.println(); //Prints a line into the serial monitor.
 
-delay(1000);// I put this here just to make the terminal 
-           // window happier
+delay(1000);// Waits a second to slow down spam in the serial monitor;
 }
 
 
